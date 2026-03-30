8b60bb56775e80efa66cafed95a06b27f06ab749
Nikunj Gupta <nikunjgupta2136@gmail.com>
2026-03-30 13:10:04 +0530

    Update : Added Field Descriptions

--- ISSR_3/backend/models.py
+++ ISSR_3/backend/models.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class EventLog(BaseModel):
-    participant_id: str
-    condition: str
-    decision: str
-    timestamp: str
-    latency_ms: float
+    participant_id: str = Field(..., description="User ID")
+    condition: str = Field(..., description="Experiment group")
+    decision: str = Field(..., description="User decision")
+    timestamp: str = Field(..., description="Event time")
+    latency_ms: float = Field(..., description="Decision time (ms)")
